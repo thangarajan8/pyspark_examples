7c8291f6f4cddda0e6aa74007fb4ac088452bd07
P <Thangarajan.P@nielsen.com>
2017-10-20 14:07:19 +0530

    data_added

--- examples/pyspark_aggregate.py
+++ examples/pyspark_aggregate.py
@@ -3,7 +3,7 @@ from pyspark.sql import SQLContext
 conf1 = SparkConf().setAppName('Aggr')
 sc1 = SparkContext(conf=conf1)
 sql_context = SQLContext(sc1)
-csv_file_path = 'C:\\Thanga\\Work\\python\\uni\\pyspark_examples\\emp.csv'
+csv_file_path = '../data/emp.csv'
 employee_rdd = sc1.textFile(csv_file_path).map(lambda line : line.split(','))
 employee_df = employee_rdd.toDF(['dept','ctc'])
 employee_df.show()
--- examples/sort_desc.py
+++ examples/sort_desc.py
@@ -3,7 +3,7 @@ from pyspark.sql import SQLContext
 conf1 = SparkConf().setAppName('sort_desc')
 sc1 = SparkContext(conf=conf1)
 sql_context = SQLContext(sc1)
-csv_file_path = 'C:\\Thanga\\Work\\python\\uni\\pyspark_examples\\emp.csv'
+csv_file_path = '../data/emp.csv'
 employee_rdd = sc1.textFile(csv_file_path).map(lambda line: line.split(','))
 print(type(employee_rdd))
 employee_rdd_sorted = employee_rdd.sortByKey(ascending= False)
